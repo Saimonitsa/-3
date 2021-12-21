e4d867b7e3224bc3686e13a3fd90bb338075e537
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-21 20:10:16 +0500

    p10

--- лаба ооп3/лаба ооп3.cpp
+++ лаба ооп3/лаба ооп3.cpp
@@ -15,27 +15,26 @@ public:
 };
 class Square : public Shape {
 private:
-	int p1, p2;
+	int p1, p2, p3;
 public: 
-
 	Square() {
-
 		p1 = 0;
 		p2 = 0;
+		p3 = 0;
 		cout << "Square()" << endl;
 	}
 
-	Square(int x, int y) {
-
+	Square(int x, int y, int z) {
 		this->p1 = x;
 		this->p2 = y;
+		this->p3 = z;
 		cout << "Square(int x, int y)" << endl;
 	}
 
 	Square(const Square& p) {
-
 		p1 = p.p1;
 		p2 = p.p2;
+		p3 = p.p3;
 		cout << "Square(const Square& p)" << endl;
 	}
 
@@ -59,27 +58,26 @@ public:
 
 class Triangle : public Shape {
 private:
-	int p1, p2;
+	int p1, p2, p3;
 public:
-
 	Triangle() {
-
 		p1 = 0;
 		p2 = 0;
+		p3 = 0;
 		cout << "Triangle()" << endl;
 	}
 
-	Triangle(int x, int y) {
-
+	Triangle(int x, int y, int z) {
 		this->p1 = x;
 		this->p2 = y;
+		this->p3 = z;
 		cout << "Triangle(int x, int y)" << endl;
 	}
 
 	Triangle(const Triangle& p) {
-
 		p1 = p.p1;
 		p2 = p.p2;
+		p3 = p.p3;
 		cout << "Triangle(const Triangle& p)" << endl;
 	}
 
@@ -196,7 +194,6 @@ public:
 		cin >> x;
 		switch (x) {
 			case 1: {
-
 				Triangle* t = new Triangle;
 				t->show_parametri();
 				t->formula();
@@ -217,25 +214,24 @@ public:
 	}
 
 	void Rand(int l) {
-
 		int x = 1 + rand() % 2;
 		switch (x) {
-
 			case 1: {
-
-				int p1, p2;
+				int p1, p2, p3;
 				p1 = 1 + rand() % 2;
 				p2 = 1 + rand() % 2;
-				Triangle* t = new Triangle(p1, p2);
+				p3 = 1 + rand() % 2;
+				Triangle* t = new Triangle(p1, p2, p3);
 				SetObject(l, t);
 				break;
 			}
 
 			case 2: {
-				int p1, p2;
+				int p1, p2, p3;
 				p1 = 1 + rand() % 2;
 				p2 = 1 + rand() % 2;
-				Square* s = new Square(p1, p2);
+				p3 = 1 + rand() % 2;
+				Square* s = new Square(p1, p2, p3);
 				SetObject(l, s);
 				break;
 			}
@@ -318,12 +314,14 @@ int main() {
 		system("cls");
 		cout << "Размер хранилища: " << storage.getCount() << endl;
 		if (storage.getCount() == 0) {
-			cout << "1 - добавить элемент" << endl << "2 - подобрать случайно" << endl << "3 - выход" << endl;
+			cout << "1 - добавить элемент" << endl << "6 - подобрать случайно" << endl << "7 - выход" << endl;
 			l = -1;
 		}
 
 		else {
-
+			storage.print(l);
+			storage.outpMax_size();
+			cout << "1 - добавить элемент" << endl << "2 - удалить" << endl << "3 - изменить" << endl << "4 - перейти к следующему" << endl << "5 - перейти к предыдущему" << endl << "6 - подобрать случайно" << endl << "7 - выход" << endl;
 		}
 		int x;
 		cin >> x;
@@ -335,13 +333,33 @@ int main() {
 				break;
 			}
 			case 2: {
-				storage.random();
+				storage.remove(l);
+				l--;
 				break;
 			}
 			case 3: {
+				storage.Cin(l);
+				break;
+			}
+			case 4: {
+				if (l + 1 < storage.getCount())
+					l++;
+				break;
+			}
+			case 5: {
+				if (l > 0)
+					l--;
+				break;
+			}
+			case 6: {
+				storage.random();
+				break;
+			}
+			case 7: {
 				flag = false;
 				break;
 			}
 		}
-	}	
+	}
+	return 0;
 }
